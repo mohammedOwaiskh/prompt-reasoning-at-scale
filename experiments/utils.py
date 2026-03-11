71f91a5c06137af4f3a4d47c42c54a6ed88da4a3
Mohammed Owais Khan <mohdowaiskh@gmail.com>
2026-03-11 21:23:46 +0100

    Add support for nf4 quantization in load_model function

--- experiments/utils.py
+++ experiments/utils.py
@@ -45,6 +45,7 @@ def load_model(model_name: str) -> tuple:
             load_in_4bit=True,
             bnb_4bit_compute_dtype=torch.float16,
             bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
         )
         model = AutoModelForCausalLM.from_pretrained(
             model_id,
